00001f0f11d857638b6af70389b673453b7253b0
Di_Masta <vvladoh@gmail.com>
2017-09-06 11:24:11 +0300

    New files for constnats and includes

---
+++ constants.cpp
@@ -0,0 +1,3 @@
+const int INVALID_ID = -1;
+const int INVALID_NODE_DEPTH = -1;
+const int TREE_ROOT_NODE_DEPTH = 1;
---
+++ includes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <list>
+#include <stack>
+#include <set>
+#include <queue>
+#include <algorithm>
+
+using namespace std;

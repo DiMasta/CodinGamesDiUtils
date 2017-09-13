6ae89b2b1f827895b645b4e79bebffd37f472320
Vladislav Vulchev <vladislav.vulchev@chaosgroup.com>
2017-09-13 08:03:19 +0300

    Utils updated

--- constants.cpp
+++ constants.cpp
@@ -2,3 +2,5 @@ const int INVALID_ID = -1;
 const int INVALID_NODE_DEPTH = -1;
 const int TREE_ROOT_NODE_DEPTH = 1;
 const int ZERO_CHAR = '0';
+const int INVALID_COORD = -1;
+const int DIRECTION_COUNT = 8;

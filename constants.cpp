091ed92c230eb3d3c62f6203e15477720723199a
Di_Masta <vvladoh@gmail.com>
2017-11-17 19:33:02 +0200

    Added entity class

--- constants.cpp
+++ constants.cpp
@@ -5,5 +5,4 @@ const int INVALID_ID = -1;
 const int INVALID_NODE_DEPTH = -1;
 const int TREE_ROOT_NODE_DEPTH = 1;
 const int ZERO_CHAR = '0';
-const int INVALID_COORD = -1;
 const int DIRECTIONS_COUNT = 8;

204b35d6888a67443cabd0b077d8f55cd8e0a71d
Vladislav Vulchev <vvladoh@gmail.com>
2020-09-10 12:35:15 +0300

    Python tricks

--- python_tricks.py
+++ python_tricks.py
@@ -14,3 +14,6 @@ def isPrime(n):
 	
 n=input()
 print(str(int(n)%sum(int(x) for x in n)==0).lower())
+
+def isPrime(n):
+    return n>1 and all(n%i for i in itertools.islice(itertools.count(2),int(math.sqrt(n)-1)))

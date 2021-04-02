68eee4b0f53f797929436df49d612cf01f955044
Di_Masta <vvladoh@gmail.com>
2021-04-02 08:27:05 +0300

    else

--- includes.cpp
+++ includes.cpp
@@ -4,12 +4,14 @@
 //#define DEBUG_ONE_TURN
 
 #ifndef REDIRECT_INPUT
+
 #pragma GCC optimize("O3","unroll-loops","omit-frame-pointer","inline") //Optimization flags
 #pragma GCC option("arch=native","tune=native","no-zero-upper") //Enable AVX
 #pragma GCC target("avx")  //Enable AVX
 #include <x86intrin.h> //AVX/SSE Extensions
 #include <bits/stdc++.h> //All main STD libraries
-#endif // REDIRECT_INPUT
+
+#else
 
 #include <iostream>
 #include <string>
@@ -32,5 +34,7 @@
 #include <iomanip>
 #include <chrono>
 #include <cassert>
+
+#endif // REDIRECT_INPUT
 
 using namespace std;
